6efd40c56079b862f0f1718921121789fbc5301f
Mori <yuhua.zhao@dragonplus.com>
2022-01-29 17:50:09 +0800

    update wait

--- frontend/entry.js
+++ frontend/entry.js
@@ -64,55 +64,51 @@ async function inject_func(noti_list) {
 
         // create new noti
         await document.getElementsByClassName("newCampaign")[0].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // fill up noti content
         document.getElementsByClassName("message-title")[0].value = noti.title;
         document.getElementsByClassName("message-title")[0].dispatchEvent(new Event("input"));
-        await sleep(1.5)
         document.getElementsByClassName("message-text")[0].value = noti.desc;
         document.getElementsByClassName("message-text")[0].dispatchEvent(new Event("input"));
-        await sleep(1.5)
         document.getElementsByClassName("notification-image-input")[0].value = noti.pic;
         document.getElementsByClassName("notification-image-input")[0].dispatchEvent(new Event("input"));
-        await sleep(1.5)
         document.getElementsByClassName("message-label")[0].value = noti.name;
         document.getElementsByClassName("message-label")[0].dispatchEvent(new Event("input"));
-        await sleep(1.5)
 
         // next
         document.getElementsByClassName("mat-stepper-next")[0].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // 触发选择app
         console.log("触发选择app")
         document.getElementsByClassName("mat-select-value")[1].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // 选择第一个app
         console.log("选择第一个app")
         document.getElementsByClassName("mat-option")[0].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // 添加第一个条件
         console.log("添加第一个条件")
         document.getElementsByClassName("and-button")[0].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // 触发添加条件
         console.log("触发添加条件")
         document.getElementsByTagName("fire-rx-targeting-options")[1].children[0].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // 选择语言条件
         console.log("选择语言条件")
         document.getElementsByClassName("mat-menu-item")[1].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // 触发选择语言
         console.log("触发选择语言")
         document.getElementsByClassName("mat-select-value")[3].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // 选择语言
         var lans = document.getElementsByClassName("mat-option")
@@ -127,47 +123,47 @@ async function inject_func(noti_list) {
             }
             if (flag) {
                 lan.click()
-                await sleep(1.5)
+                // await sleep(0.8)
             }
         }
-        await sleep(1.5)
+        await sleep(0.8)
         // 关闭弹框
         document.getElementsByClassName("cdk-overlay-backdrop")[0].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // 添加第二个app
         document.getElementsByClassName("add-app")[0].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // 触发选择第二个app
         console.log("触发选择第二个app")
         document.getElementsByClassName("mat-select-value")[4].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // 选择第二个app
         console.log("选择第二个app")
         document.getElementsByClassName("mat-option")[0].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // 添加第二套的条件
         console.log("添加第二套的条件")
         document.getElementsByClassName("and-button")[2].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // 触发第二套的添加语言
         console.log("触发第二套的添加语言")
         document.getElementsByTagName("fire-rx-targeting-options")[3].children[0].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // 添加第二套语言条件
         console.log("添加第二套语言条件")
         document.getElementsByClassName("mat-menu-item")[1].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // 触发选择语言
         console.log("触发选择语言")
         document.getElementsByClassName("mat-select-value")[6].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // 选择语言
         var lans = document.getElementsByClassName("mat-option")
@@ -182,31 +178,31 @@ async function inject_func(noti_list) {
             }
             if (flag) {
                 lan.click()
-                await sleep(1.5)
+                // await sleep(0.8)
             }
         }
         // 关闭弹框
         document.getElementsByClassName("cdk-overlay-backdrop")[0].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // next
         document.getElementsByClassName("mat-stepper-next")[1].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // 触发时间选择
         console.log("触发时间选择")
         document.getElementsByTagName("fcm-datepicker")[0].getElementsByTagName("input")[0].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // 选择时间
         console.log("选择时间")
         document.getElementsByClassName("mat-menu-item")[1].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // 触发日期选择
         console.log("触发日期选择")
         document.getElementsByClassName("mat-calendar-period-button")[0].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // 选年
         console.log("选年")
@@ -215,10 +211,10 @@ async function inject_func(noti_list) {
             let calendar_year_cell = calendar_year_cells.item(index)
             if (calendar_year_cell.textContent.trim() === noti.push_date.year) {
                 calendar_year_cell.click()
-                await sleep(1.5)
                 break
             }
         }
+        await sleep(0.8)
 
         // 选月
         console.log("选月")
@@ -227,10 +223,10 @@ async function inject_func(noti_list) {
             let calendar_month_cell = calendar_month_cells.item(index)
             if (calendar_month_cell.textContent.trim() === noti.push_date.month.str) {
                 calendar_month_cell.click()
-                await sleep(1.5)
                 break
             }
         }
+        await sleep(0.8)
 
         // 选日
         console.log("选日")
@@ -239,20 +235,21 @@ async function inject_func(noti_list) {
             let calendar_day_cell = calendar_day_cells.item(index)
             if (calendar_day_cell.textContent.trim() === noti.push_date.day) {
                 calendar_day_cell.click()
-                await sleep(1.5)
                 break
             }
         }
+        await sleep(0.8)
 
         // 输入时间
         console.log("输入时间")
-        document.getElementsByClassName("c5e-input-container")[6].children[0].value = `${noti.push_time.hour}:${noti.push_time.minute}`
-        await sleep(1.5)
+        document.getElementsByTagName("fcm-datetimepicker")[0].getElementsByTagName("input")[1].value = `${noti.push_time.hour}:${noti.push_time.minute}`
+        document.getElementsByTagName("fcm-datetimepicker")[0].getElementsByTagName("input")[1].dispatchEvent(new Event("input"))
+        // await sleep(0.8)
 
         // 触发时区
         console.log("触发时区")
         document.getElementsByClassName("fcm-timezonepicker-button")[0].click()
-        await sleep(1.5)
+        // await sleep(0.8)
 
         // 时区
         console.log("选时区")
@@ -261,99 +258,77 @@ async function inject_func(noti_list) {
             let btn = btns.item(index)
             if (btn.textContent === "Recipient time zone") {
                 btn.click()
-                await sleep(1.5)
                 break
             }
         }
+        await sleep(0.8)
 
         // next
         document.getElementsByClassName("mat-stepper-next")[2].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         document.getElementsByClassName("mat-stepper-next")[3].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         // kv
         document.getElementsByClassName("abt-message-options-form")[0].getElementsByTagName("input")[1].value = "push_from"
         document.getElementsByClassName("abt-message-options-form")[0].getElementsByTagName("input")[1].dispatchEvent(new Event("input"));
-        await sleep(1.5)
 
         document.getElementsByClassName("abt-message-options-form")[0].getElementsByTagName("input")[2].value = "FcmPush"
         document.getElementsByClassName("abt-message-options-form")[0].getElementsByTagName("input")[2].dispatchEvent(new Event("input"));
-        await sleep(1.5)
 
         document.getElementsByClassName("abt-message-options-form")[0].getElementsByTagName("input")[3].value = "notify_id"
         document.getElementsByClassName("abt-message-options-form")[0].getElementsByTagName("input")[3].dispatchEvent(new Event("input"));
-        await sleep(1.5)
 
         document.getElementsByClassName("abt-message-options-form")[0].getElementsByTagName("input")[4].value = `${noti.push_date.year}${noti.push_date.month.int}${noti.push_date.day}`
         document.getElementsByClassName("abt-message-options-form")[0].getElementsByTagName("input")[4].dispatchEvent(new Event("input"));
-        await sleep(1.5)
 
         document.getElementsByClassName("abt-message-options-form")[0].getElementsByTagName("input")[5].value = "notify_time"
         document.getElementsByClassName("abt-message-options-form")[0].getElementsByTagName("input")[5].dispatchEvent(new Event("input"));
-        await sleep(1.5)
 
         document.getElementsByClassName("abt-message-options-form")[0].getElementsByTagName("input")[6].value = `${noti.push_time.hour}${noti.push_time.minute}`
         document.getElementsByClassName("abt-message-options-form")[0].getElementsByTagName("input")[6].dispatchEvent(new Event("input"));
-        await sleep(1.5)
+        await sleep(0.8)
 
         document.getElementsByClassName("mat-select-value")[7].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
         var options = document.getElementsByClassName("mat-option")
         for (let index = 0; index < options.length; index++) {
             let option = options.item(index)
             if (option.textContent.trim() == 'Enabled') {
                 option.click()
-                await sleep(1.5)
                 break
             }
         }
+        await sleep(0.8)
 
         document.getElementsByClassName("mat-select-value")[8].click()
-        await sleep(1.5)
+        await sleep(0.8)
+
         var options = document.getElementsByClassName("mat-option")
         for (let index = 0; index < options.length; index++) {
             let option = options.item(index)
             if (option.textContent.trim() == 'Enabled') {
                 option.click()
-                await sleep(1.5)
                 break
             }
         }
+        await sleep(0.8)
 
         document.getElementsByClassName("badge-count-input")[0].value = 1
-        await sleep(1.5)
+        document.getElementsByClassName("badge-count-input")[0].dispatchEvent(new Event("input"))
+        // await sleep(0.8)
 
         document.getElementsByClassName("cta-bottom")[0].getElementsByTagName("button")[1].click()
-        await sleep(1.5)
+        await sleep(0.8)
 
-        document.getElementsByClassName("fire-dialog-actions")[0].getElementsByTagName("button")[1].click()
-        await sleep(15)
-
-
-        chrome.notifications.create(
-            "FCM noti",
-            {
-                iconUrl: "icon.png",
-                type: "basic",
-                title: "啵啵提醒你",
-                message: noti.name + " - 填写完成",
-            },
-        )
-        // next
-        // var btns = document.getElementsByClassName("mat-stepper-next")
-        // for (let index = 0; index < btns.length; index++) {
-        //     var btn = btns.item(index)
-        //     if (btn.textContent === "Next") {
-        //         btn.click()
-        //     }
-        // }
-        // await sleep(2000)
         // return
-        // document.getElementsByClassName("mat-menu-trigger")[0].click()
+        document.getElementsByClassName("fire-dialog-actions")[0].getElementsByTagName("button")[1].click()
+        await sleep(10)
+
     }
+    alert("Shift Bro, 填完了");
 
 }
 
